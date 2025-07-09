5f22f0cf1987a0b96d4a8a5fdec0f15f3b0142d1
aaqibraza92 <78475535+aaqibraza92@users.noreply.github.com>
2025-07-09 16:02:14 +0530

    u

--- src/pages/blogs/blogItems.js
+++ src/pages/blogs/blogItems.js
@@ -61,7 +61,7 @@ const BlogItems = () => {
     };
 
     await axios
-      .get(HOME_URL+CAT_URL,
+      .get(HOME_URL + CAT_URL,
         options
       )
       .then((res) => {
@@ -79,7 +79,7 @@ const BlogItems = () => {
       },
     };
 
-    await axios.get(`${HOME_URL+BLOG_URL}?per_page=18&page=${currentPage}`, options).then((res) => {
+    await axios.get(`${HOME_URL + BLOG_URL}?per_page=18&page=${currentPage}`, options).then((res) => {
       if (res && res.status === 200) {
         setPostData(res?.data);
         setloading(false);
@@ -94,60 +94,60 @@ const BlogItems = () => {
   };
 
 
-const truncateHTML = (content, limit) => {
-  const words = content.split(' ');
-  const truncatedWords = words.slice(0, limit);
-  return truncatedWords.join(' ');
-};
+  const truncateHTML = (content, limit) => {
+    const words = content.split(' ');
+    const truncatedWords = words.slice(0, limit);
+    return truncatedWords.join(' ');
+  };
 
 
   return (
     <>
-          {loading && (
-     <Row className="gy-4">
-      {
-        Array(12).fill().map((_,ind)=>(
-  <Col md={4}>
-    <div className="skeleton" style={{width: '100%', height: "200px"}}>
+      {loading && (
+        <Row className="gy-4">
+          {
+            Array(12).fill().map((_, ind) => (
+              <Col md={4} key={ind}>
+                <div className="skeleton" style={{ width: '100%', height: "200px" }}>
 
-    </div>
-        </Col>
-        ))
-      }
-      
-     </Row>
+                </div>
+              </Col>
+            ))
+          }
+
+        </Row>
       )}
 
-      
+
       <section className="mt80">
 
-          {
-              currentPage===1 &&  <Row>
-              <Col lg={8} md={8}>
-                {
-                  screenWidth > 992 &&       <div className="blogWrapper mb30 d-flex align-items-center radius12 position-relative pb0 transition">
+        {
+          currentPage === 1 && <Row>
+            <Col lg={8} md={8}>
+              {
+                screenWidth > 992 && <div className="blogWrapper mb30 d-flex align-items-center radius12 position-relative pb0 transition">
                   <div className="featuredImg0">
-                  <Link to={`/blog/${postData?.[0]?.slug}`}> 
-                  <img
-                      src={postData?.[0]?.fimg_url}
-                      className="img-fluid w-100 radius12"
-                      alt=""
-                    /></Link>
-                   
+                    <Link to={`/blog/${postData?.[0]?.slug}`}>
+                      <img
+                        src={postData?.[0]?.fimg_url}
+                        className="img-fluid w-100 radius12"
+                        alt=""
+                      /></Link>
+
                   </div>
                   <div className="overlaypText radius text-start pt20 pb20 pl20 pr20">
-                    <h3 className="lh24" title={postData?.[0]?.title?.rendered}>   <Link to={`/blog/${postData?.[0]?.slug}`} className="fs18 fBold colorBlue"> {postData?.[0]?.title?.rendered.slice(0,33)} ... </Link></h3>
-                    <div className="mt8 colorPara" dangerouslySetInnerHTML={{ __html:  truncateHTML(postData && postData?.[0]?.content?.rendered, 25)  }} />
+                    <h3 className="lh24" title={postData?.[0]?.title?.rendered}>   <Link to={`/blog/${postData?.[0]?.slug}`} className="fs18 fBold colorBlue"> {postData?.[0]?.title?.rendered.slice(0, 33)} ... </Link></h3>
+                    <div className="mt8 colorPara" dangerouslySetInnerHTML={{ __html: truncateHTML(postData && postData?.[0]?.content?.rendered, 25) }} />
                     <div className="d-flex align-items-center">
                       <span>{timeIcon}</span> <span className="fs14 colorPara ml5">  {dateConverter(postData?.[0]?.modified)}</span>
                     </div>
                   </div>
                 </div>
-                }
-        
-              </Col>
-              <Col lg={4} md={12}>
-                {/* <div className="mb30">
+              }
+
+            </Col>
+            <Col lg={4} md={12}>
+              {/* <div className="mb30">
                 <input
                   type="text"
                   className="inputTheme w-100"
@@ -159,88 +159,88 @@ const truncateHTML = (content, limit) => {
                 </button>
                 </div>
                 </div> */}
-            
-  
-                <div className="insight radius12 mb30">
+
+
+              <div className="insight radius12 mb30">
                 <h4 className="fs20 colorBlue fBold pt20 pb20 pl20 pr20 mb0 borderBottom">
-                Insights
+                  Insights
                 </h4>
                 <ul className="catUl pt20 pb20 pl20 pr20 ">
-                {
-                  allCategory.length > 0 && allCategory.map((e,i)=>(
-                    e.name!=='Uncategorized' &&
-                    <li key={i} className="mb10">
-                      <Link className="fs15 colorBlue" to={`/category/${e?.id}`}>{e?.name}</Link>
-                    </li>
-                  ))
-                }
+                  {
+                    allCategory.length > 0 && allCategory.map((e, i) => (
+                      e.name !== 'Uncategorized' &&
+                      <li key={i} className="mb10">
+                        <Link className="fs15 colorBlue" to={`/category/${e?.id}`}>{e?.name}</Link>
+                      </li>
+                    ))
+                  }
                 </ul>
-          
-             
-                </div>
-              </Col>
-            </Row>
-          }
-       
-       
+
+
+              </div>
+            </Col>
+          </Row>
+        }
+
+
       </section>
 
-   <Row className="gy-4 gx-4 mb60">
-      {postData.length > 0 && postData?.map((e, i) => (
-        screenWidth > 1024 ? (  currentPage===1 && i!==0)  &&
-          <Col lg={4} md={6} xs={12} key={i}>
-            <div className="blogWrapper radius12 pb15 transition">
-              <div className="featuredImg">
-              <Link to={`/blog/${e?.slug}`}> <img
-                  src={e?.fimg_url}
-                  className="img-fluid w-100"
-                  alt=""
-                /></Link>
-               
-              </div>
-              <div className="text-start pt20 pb20 pl20 pr20">
-                <h3 className="lh24" title={e?.title?.rendered}>   <Link to={`/blog/${e?.slug}`} className="fs18 fBold colorBlue"> {e?.title?.rendered.slice(0,33)} ... </Link></h3>
-              
-                <div className="mt8 colorPara" dangerouslySetInnerHTML={{ __html:  truncateHTML(e?.content?.rendered, 25)  }} />
-                <div className="d-flex align-items-center">
-                  <span>{timeIcon}</span> <span className="fs14 colorPara ml5">  {dateConverter(e?.modified)}</span>
+      <Row className="gy-4 gx-4 mb60">
+        {postData.length > 0 && postData?.map((e, i) => (
+          screenWidth > 1024 ? (currentPage === 1 && i !== 0) &&
+            <Col lg={4} md={6} xs={12} key={i}>
+              <div className="blogWrapper radius12 pb15 transition">
+                <div className="featuredImg">
+                  <Link to={`/blog/${e?.slug}`}> <img
+                    src={e?.fimg_url}
+                    className="img-fluid w-100"
+                    alt=""
+                  /></Link>
+
+                </div>
+                <div className="text-start pt20 pb20 pl20 pr20">
+                  <h3 className="lh24" title={e?.title?.rendered}>   <Link to={`/blog/${e?.slug}`} className="fs18 fBold colorBlue"> {e?.title?.rendered.slice(0, 33)} ... </Link></h3>
+
+                  <div className="mt8 colorPara" dangerouslySetInnerHTML={{ __html: truncateHTML(e?.content?.rendered, 25) }} />
+                  <div className="d-flex align-items-center">
+                    <span>{timeIcon}</span> <span className="fs14 colorPara ml5">  {dateConverter(e?.modified)}</span>
+                  </div>
                 </div>
               </div>
-            </div>
-          </Col>
-          :
-          <Col lg={4} md={6} xs={12} key={i}>
-          <div className="blogWrapper radius12 pb15 transition">
-            <div className="featuredImg">
-            <Link to={`/blog/${e?.slug}`}> <img
-                src={e?.x_featured_media_medium}
-                className="img-fluid w-100"
-                alt=""
-              /></Link>
-             
-            </div>
-            <div className="text-start pt20 pb20 pl20 pr20">
-              <h3 className="lh24" title={e?.title?.rendered}>   <Link to={`/blog/${e?.slug}`} className="fs18 fBold colorBlue"> {e?.title?.rendered.slice(0,33)} ... </Link></h3>
-            
-              <div className="mt8 colorPara" dangerouslySetInnerHTML={{ __html:  truncateHTML(e?.content?.rendered, 25)  }} />
-              <div className="d-flex align-items-center">
-                <span>{timeIcon}</span> <span className="fs14 colorPara ml5">  {dateConverter(e?.modified)}</span>
-              </div>
-            </div>
-          </div>
-        </Col>
-        ))}
-    </Row>
+            </Col>
+            :
+            <Col lg={4} md={6} xs={12} key={i}>
+              <div className="blogWrapper radius12 pb15 transition">
+                <div className="featuredImg">
+                  <Link to={`/blog/${e?.slug}`}> <img
+                    src={e?.x_featured_media_medium}
+                    className="img-fluid w-100"
+                    alt=""
+                  /></Link>
 
-           <Pagination
-          {...bootstrap5PaginationPreset}
-          current={Number(currentPage)}
-          total={Number(totalPage)}
-          onPageChange={setCurrentPageHandle}
-       
-        />
+                </div>
+                <div className="text-start pt20 pb20 pl20 pr20">
+                  <h3 className="lh24" title={e?.title?.rendered}>   <Link to={`/blog/${e?.slug}`} className="fs18 fBold colorBlue"> {e?.title?.rendered.slice(0, 33)} ... </Link></h3>
+
+                  <div className="mt8 colorPara" dangerouslySetInnerHTML={{ __html: truncateHTML(e?.content?.rendered, 25) }} />
+                  <div className="d-flex align-items-center">
+                    <span>{timeIcon}</span> <span className="fs14 colorPara ml5">  {dateConverter(e?.modified)}</span>
+                  </div>
+                </div>
+              </div>
+            </Col>
+        ))}
+      </Row>
+
+      <Pagination
+        {...bootstrap5PaginationPreset}
+        current={Number(currentPage)}
+        total={Number(totalPage)}
+        onPageChange={setCurrentPageHandle}
+
+      />
     </>
- 
+
   );
 };
 
